0064bd41594f74dd2f11d0d25e906bd27587d818
elham715 <elham4ml.ai@gmail.com>
2025-09-22 17:20:07 +0600

    Create newtonia_mechanics.js

---
+++ questions/newtonia_mechanics.js
@@ -0,0 +1,21 @@
+// Newtonian Mechanics Exam exam questions
+// Generated on 9/22/2025
+// Total questions: 1
+
+const questions = [
+    {
+        "id": "q_1758539961875_kpqjoelv0",
+        "text": "j",
+        "topic": "df",
+        "difficulty": "intermediate",
+        "image": "",
+        "video": "https://www.youtube.com/watch?v=example_df",
+        "options": {
+            "A": "dsf",
+            "B": "dfs",
+            "C": "fds",
+            "D": "fs"
+        },
+        "correct": "A"
+    }
+];
